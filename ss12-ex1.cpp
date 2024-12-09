fdf2830395b0ee804434aafc6af414cb32e31ec8
empedangiunhatptit <phamngoclinhtq2006@gmail.com>
2024-12-09 19:11:42 +0700

    Add files via upload

---
+++ ss12-ex1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int number1, number2;
+    int sum;
+	    printf("1. Tinh tong 2 so\n");
+	    		printf("Moi ban nhap so thu nhat: ");
+	    		scanf("%d", &number1);
+	    		printf("Moi ban nhap so thu hai: ");
+	    		scanf("%d", &number2);
+	    		sum = number1 + number2;
+	    		printf("Tong 2 so = %d\n", sum);
+	
+    return 0;
+}
